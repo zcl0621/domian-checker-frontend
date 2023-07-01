a6843a15b1239c670fa5cf8d9d4210ac9599731b
zcl0621 <zcl0621@hotmail.com>
2023-07-01 23:47:17 +0800

    去除域名判断

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 npm run build && \
-docker buildx build --platform linux/amd64 -t dns-frontend:17 . && \
-docker save -o frontend-17.tar dns-frontend:17 && \
-scp frontend-17.tar dns_vps:/tmp/frontend-17.tar && \
-rm -rf frontend-17.tar && \
-docker rmi dns-frontend:17
+docker buildx build --platform linux/amd64 -t dns-frontend:18 . && \
+docker save -o frontend-18.tar dns-frontend:18 && \
+scp frontend-18.tar dns_vps:/tmp/frontend-18.tar && \
+rm -rf frontend-18.tar && \
+docker rmi dns-frontend:18
